3b43259144c1845060375444aeb5797e950e11e5
satyasravan27 <71518994+satyasravan27@users.noreply.github.com>
2020-09-18 22:27:15 +0530

    created hello.java program
    This file prints hello on the screen

---
+++ hello.java
@@ -0,0 +1,9 @@
+package com.sravan;
+
+public class Main{                                  //intialization of Main class
+
+    public static void main(String[] args){        //intialization of main method in Main class
+    
+    system.out.println("hello");
+    }
+}
